cf598318a3f796054a80c7528e04d8b7a97c163e
RishikaRajput25 <itsrishikarajput@gmail.com>
2025-03-21 11:48:01 +0530

    database connected

---
+++ src/index.js
@@ -0,0 +1,38 @@
+
+//2nd approach
+import connectDB from "./db/indexdb.js";
+import dotenv from "dotenv";
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+//1st approch in which index.js contain all connectivity code
+/*
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app=express()
+(async()=>{//it is an ifies
+try {
+     await mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`)
+     app.on("error",(error)=>{
+        console.log("ERROR :",error)
+        throw error;
+     })
+     app.listen(process.env.PORT,()=>{
+        console.log(`App is listenning on port ${process.env.PORT}`);
+     })
+} catch (error) {
+    console.error("ERROR:",error)
+    throw err
+}
+})()  */
